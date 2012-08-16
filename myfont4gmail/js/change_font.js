98841a2a4eb8bc49dd6dad142840277d503b3b1a
Lance Jian <lancejian@fortinet.com>
2012-08-16 09:58:03 -0700

    0.1.7

--- myfont4gmail/js/change_font.js
+++ myfont4gmail/js/change_font.js
@@ -1,9 +1,8 @@
 var mfg_font_added = false;
 document.addEventListener("DOMNodeInserted", function(e){
   if (!mfg_font_added ) {
-      var styles = document.getElementById("canvas_frame"
-      ).contentDocument.styleSheets;
-      if (styles.length == 2) {
+      var styles = document.getElementById("canvas_frame").contentDocument.styleSheets;
+      if (styles.length > 0) {
           mfg_font_added = true;
           chrome.extension.sendRequest({method: "getFont"}, function(response) {
             var rule = "", family = response.family, size = response.size;
@@ -14,7 +13,7 @@ document.addEventListener("DOMNodeInserted", function(e){
               rule += "font-size:" + size  + "pt!important;";
             }
             if (rule) {
-              styles[1].addRule(".ii,.Ak", rule);
+              styles[styles.length - 1].addRule(".ii,.Ak", rule);
             }
           });
       }
