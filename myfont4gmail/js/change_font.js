7019f05bcaa297e274f30fcb5d57f743feb3d7e4
Lance Jian <lancelotj100@gmail.com>
2012-10-08 23:55:35 -0700

    fixed not working problem

--- myfont4gmail/js/change_font.js
+++ myfont4gmail/js/change_font.js
@@ -1,21 +1,26 @@
-var mfg_font_added = false;
-document.addEventListener("DOMNodeInserted", function(e){
-  if (!mfg_font_added ) {
-      var styles = document.getElementById("canvas_frame").contentDocument.styleSheets;
-      if (styles.length > 0) {
-          mfg_font_added = true;
-          chrome.extension.sendRequest({method: "getFont"}, function(response) {
-            var rule = "", family = response.family, size = response.size;
-            if (family) {
-              rule += "font-family:\"" + family  + "\"!important;";
-            }
-            if (size) {
-              rule += "font-size:" + size  + "pt!important;";
-            }
-            if (rule) {
-              styles[styles.length - 1].addRule(".ii,.Ak", rule);
-            }
-          });
-      }
-  }
-});
+(function(){
+    var PAGE_CHANGED_TIMER = null;
+    document.body.addEventListener("DOMNodeInserted", pageBitLoaded, false);
+    function pageBitLoaded(e) {
+        if (PAGE_CHANGED_TIMER !== null) {
+            clearTimeout(PAGE_CHANGED_TIMER);
+        }
+        PAGE_CHANGED_TIMER = setTimeout(function() {
+            document.body.removeEventListener("DOMNodeInserted", pageBitLoaded, false);
+            clearTimeout(PAGE_CHANGED_TIMER);
+            chrome.extension.sendRequest({method: "getFont"}, function(response) {
+                var rule = "", family = response.family, size = response.size;
+                if (family) {
+                    rule += "font-family:\"" + family  + "\"!important;";
+                }
+                if (size) {
+                    rule += "font-size:" + size  + "pt!important;";
+                }
+                if (rule) {
+                    document.styleSheets[document.styleSheets.length - 1].addRule(".ii,.Ak", rule);
+                }
+            });
+        },
+        100);
+    }
+})();
